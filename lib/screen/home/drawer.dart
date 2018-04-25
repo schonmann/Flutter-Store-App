20a03d81b0095ce98e76bdebbbdb81966b9d2b53
schonmann <antonio_schonmann@Hotmail.com>
2018-04-25 20:00:37 -0300

    Refactor
    (~) DrawerHeader removed (using UserAccountDrawerHeader widget instead).

--- lib/screen/home/drawer.dart
+++ lib/screen/home/drawer.dart
@@ -15,131 +15,123 @@ class AppDrawer extends StatefulWidget {
 }
 
 class _AppDrawerState extends State<AppDrawer> {
-
   final SessionManager _sessionManager = new SessionManager();
 
   User _user;
 
   @override
   Widget build(BuildContext context) {
-
     this._user = _sessionManager.getCurrentUser();
 
-    return new Drawer(
-      child: new ListView(
-        children: <Widget>[
-          new DrawerHeader(
-            child: new Container(
-              padding: new EdgeInsets.symmetric(
-                vertical: 10.0,
-                horizontal: 20.0,
-              ),
-              child: new Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  new Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: <Widget>[
-                      new Hero(
-                        tag: "profile-pic",
-                        child: new CircleAvatar(
-                          backgroundImage: new NetworkImage(
-                            _user.profilePictureUrl
-                          ),
-                          maxRadius: 40.0,
-                        ),
-                      ),
-                      new Text(
-                        _user.username,
-                        style: new TextStyle(
-                          color: Colors.white,
-                          fontSize: 16.0,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-            decoration: new BoxDecoration(
-              color: Theme.of(context).primaryColor,
-            ),
-          ),
-          ListTile(
-            leading: new Icon(Icons.store),
-            title: new Text(
-              "Store",
-              style: Theme.of(context).textTheme.subhead,
-            ),
-            subtitle: new Text("Find group buying deals you may like!"),
-            onTap: () {
-              Navigator.of(context).pop();
-              Navigator.of(context).push(
+    var _drawerHeader = new UserAccountsDrawerHeader(
+      accountName: new Text(
+        _user.username,
+        textAlign: TextAlign.start,
+        style: new TextStyle(
+          color: Theme.of(context).secondaryHeaderColor,
+          fontSize: 12.0,
+        ),
+      ),
+      accountEmail: new Text(
+        _user.email,
+        textAlign: TextAlign.start,
+        style: new TextStyle(
+          height: 1.0,
+          color: Theme.of(context).primaryColorLight,
+          fontSize: 12.0,
+        ),
+      ),
+      currentAccountPicture: new Hero(
+        tag: "profile-pic",
+        child: new CircleAvatar(
+          backgroundImage: new NetworkImage(_user.profilePictureUrl),
+          maxRadius: 45.0,
+        ),
+      ),
+      decoration: new BoxDecoration(
+        color: Theme.of(context).primaryColor,
+      ),
+    );
+
+    var _drawerListItems = <Widget>[
+      ListTile(
+        leading: new Icon(Icons.store),
+        title: new Text(
+          "Store",
+          style: Theme.of(context).textTheme.subhead,
+        ),
+        subtitle: new Text("Find group buying deals you may like!"),
+        onTap: () {
+          Navigator.of(context).pop();
+          Navigator.of(context).push(
                 new MaterialPageRoute(
                   builder: (BuildContext context) => new ShopPage(),
                 ),
               );
-            },
-          ),
-          ListTile(
-            leading: new Icon(Icons.info),
-            title: new Text(
-              "My Profile",
-              style: Theme.of(context).textTheme.subhead,
-            ),
-            subtitle: new Text("Billing info, general settings and more."),
-            onTap: () {
-              Navigator.of(context).pop();
-              Navigator.of(context).push(
+        },
+      ),
+      ListTile(
+        leading: new Icon(Icons.info),
+        title: new Text(
+          "My Profile",
+          style: Theme.of(context).textTheme.subhead,
+        ),
+        subtitle: new Text("Billing info, general settings and more."),
+        onTap: () {
+          Navigator.of(context).pop();
+          Navigator.of(context).push(
                 new MaterialPageRoute(
                   builder: (BuildContext context) => new ProfilePage(),
                 ),
               );
-            },
-          ),
-          ListTile(
-            leading: new Icon(Icons.shopping_cart),
-            title: new Text(
-              "Shopping Cart",
-              style: Theme.of(context).textTheme.subhead,
-            ),
-            subtitle: new Text("Shows the list of chosen products!"),
-            onTap: () {},
-          ),
-          ListTile(
-            leading: new Icon(Icons.thumb_up),
-            title: new Text(
-              "Recommendations",
-              style: Theme.of(context).textTheme.subhead,
-            ),
-            subtitle: new Text("Products you may like!"),
-            onTap: () {
-              Navigator.of(context).pop();
-              Navigator.of(context).push(
+        },
+      ),
+      ListTile(
+        leading: new Icon(Icons.shopping_cart),
+        title: new Text(
+          "Shopping Cart",
+          style: Theme.of(context).textTheme.subhead,
+        ),
+        subtitle: new Text("Shows the list of chosen products!"),
+        onTap: () {},
+      ),
+      ListTile(
+        leading: new Icon(Icons.thumb_up),
+        title: new Text(
+          "Recommendations",
+          style: Theme.of(context).textTheme.subhead,
+        ),
+        subtitle: new Text("Products you may like!"),
+        onTap: () {
+          Navigator.of(context).pop();
+          Navigator.of(context).push(
                 new MaterialPageRoute(
                   builder: (BuildContext context) => new RecommendationsPage(),
                 ),
               );
-            },
-          ),
-          ListTile(
-            leading: new Icon(Icons.help),
-            title: new Text(
-              "Help",
-              style: Theme.of(context).textTheme.subhead,
-            ),
-            subtitle: new Text("We may aid you :)"),
-            onTap: () {
-              Navigator.of(context).pop();
-              Navigator.of(context).push(
+        },
+      ),
+      ListTile(
+        leading: new Icon(Icons.help),
+        title: new Text(
+          "Help",
+          style: Theme.of(context).textTheme.subhead,
+        ),
+        subtitle: new Text("We may aid you :)"),
+        onTap: () {
+          Navigator.of(context).pop();
+          Navigator.of(context).push(
                 new MaterialPageRoute(
                   builder: (BuildContext context) => new HelpPage(),
                 ),
               );
-            },
-          ),
-        ],
+        },
+      ),
+    ];
+
+    return new Drawer(
+      child: new ListView(
+        children: new List<Widget>.from([_drawerHeader])..addAll(_drawerListItems),
       ),
     );
   }
